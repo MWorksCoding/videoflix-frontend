b607e9e147535efd35102bab07261a389e781b20
MWorksCoding <mail.org@posteo.org>
2024-11-01 12:14:49 +0100

    environment adjustment

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,8 +1,4 @@
-// export const environment = {
-//     production: false,
-//     baseUrl: 'https://videoflix-server.marius-katzer.com'
-//   };  
 export const environment = {
-  production: false,
-  baseUrl: 'http://127.0.0.1:8000'
-};  
+    production: false,
+    baseUrl: 'https://videoflix-server.marius-katzer.com'
+  };  
